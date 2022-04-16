0e61d8bc3b554230e57323c56472b3494118e719
KatherineQuintana <katty.quintana@gmail.com>
2022-04-16 00:25:13 -0400

    Se crea vista para estadísticas

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
       <div class="card-body">
-          <h2 class="text-success">{{cripto.name}}</h2>
+          <router-link :to="`/element/${cripto.name}`" class="text-success">{{cripto.name}}</router-link>
           <h5 class="card-text">
               <span class="badge bg-secondary mb-1">Rankin: {{cripto.rank}}</span>
               <span class="badge bg-dark d-block mb-1">Simbolo: {{cripto.symbol}}</span>
---
+++ src/components/OneCripto.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Estadistica de {{name}}</h1>
+</template>
+
+<script>
+export default {
+    props:['name'],
+    setup(props){
+
+    }
+}
+</script>
